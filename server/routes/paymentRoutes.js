265f5dcc97c7c1ad540bc63a64292da101f6d651
Hafiz13Muneeb <hafizmuneeb.ma13@gmail.com>
2026-07-07 02:20:44 -0700

    Add a temporai oute in paimentControler.js for testing onli.

--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -12,6 +12,10 @@ const { protect } = require('../middleware/auth');
 // ALL ROUTES ARE PROTECTED (require authentication)
 router.use(protect);
 
+router.get('/test', (req, res) => {
+  res.json({ message: 'Payment routes are working!' });
+});
+
 // Create a checkout session
 router.post('/create-checkout', createCheckout);
 
